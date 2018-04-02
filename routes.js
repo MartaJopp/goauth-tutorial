47a7877eb6884ed62bdcf28e16dd3cbed6526757
Marta Jopp <martapeterson@gmail.com>
2018-04-02 07:20:41 -0500

    routes

---
+++ routes.js
@@ -0,0 +1,48 @@
+module.exports = function (app, passport) {
+
+ //Home Page
+    app.get('/', function (req, res) {
+        res.render('index.ejs'); // load the index.ejs file
+    });
+
+//Login
+    app.get('/login', function (req, res) {
+
+        // render the page and pass in any flash data if it exists
+        res.render('login.ejs', { message: req.flash('loginMessage') });
+    });
+
+//Signup
+    app.get('/signup', function (req, res) {
+
+        // render the page and pass in any flash data if it exists
+        res.render('signup.ejs', { message: req.flash('signupMessage') });
+    });
+
+    // process the signup form
+    // app.post('/signup', do all our passport stuff here);
+
+//Profile
+    app.get('/profile', isLoggedIn, function (req, res) {
+        res.render('profile.ejs', {
+            user: req.user // get the user out of session and pass to template
+        });
+    });
+
+//Logout
+    app.get('/logout', function (req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+};
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+
+    // if user is authenticated in the session, carry on 
+    if (req.isAuthenticated())
+        return next();
+
+    // if they aren't redirect them to the home page
+    res.redirect('/');
+}
